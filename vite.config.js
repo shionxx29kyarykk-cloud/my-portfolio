047c9c16b40dab0da4fabe863521d32c43b4b396
shionxx29 <shionxx29kyarykk@gmail.com>
2025-10-27 13:13:32 +0900

    修正

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/',
+  base: '/my-portfolio/', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
 });
